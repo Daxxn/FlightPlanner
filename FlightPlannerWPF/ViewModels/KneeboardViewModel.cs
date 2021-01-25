7e300b4106b7741d8f1d366b6d227b19f4a1d4b8
Daxxn <nynjalantz@gmail.com>
2021-01-24 18:32:38 -0800

    Testing things only.

--- FlightPlannerWPF/ViewModels/KneeboardViewModel.cs
+++ FlightPlannerWPF/ViewModels/KneeboardViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Win32;
 using MVVMLibrary;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace FlightPlannerWPF.ViewModels
@@ -10,18 +12,59 @@ namespace FlightPlannerWPF.ViewModels
    {
       #region - Fields & Properties
       private readonly IConfiguration Config = MainViewModel.Config;
+
+      #region private props
+      private string _currentFilePath;
+      #endregion
+
+      #region Commands
+      public Command OpenTestCmd { get; private set; }
+      #endregion
       #endregion
 
       #region - Constructors
-      public KneeboardViewModel() { }
+      public KneeboardViewModel()
+      {
+         OpenTestCmd = new Command(OpenTest);
+      }
       #endregion
 
       #region - Methods
+      public void OpenTest(object p)
+      {
+         OpenFileDialog browser = new OpenFileDialog();
 
+         if (browser.ShowDialog() == true)
+         {
+            CurrentFilePath = browser.FileName;
+         }
+      }
       #endregion
 
       #region - Full Properties
 
+      public string CurrentFilePath
+      {
+         get { return _currentFilePath; }
+         set
+         {
+            _currentFilePath = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(CurrentFileName));
+         }
+      }
+
+      public string CurrentFileName
+      {
+         get
+         {
+            if (CurrentFilePath != null && File.Exists(CurrentFilePath))
+            {
+               return Path.GetFileName(CurrentFilePath);
+            }
+            return null;
+         }
+      }
       #endregion
    }
 }
